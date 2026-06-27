de715c878dc078eec0f84ab6ad8d019e0a4fa307
izumoriluik <izumoriluik@gmail.com>
2026-06-28 04:25:25 +0900

    feat: アイコンv2適用＋UIスタイル改修(フォント・配置)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'summitsirdar-v1';
+const CACHE = 'summitsirdar-v2';
 const ASSETS = ['./', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
