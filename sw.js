75de1f97b35ee781fd614468728c1a24cbc18b9e
izumoriluik <izumoriluik@gmail.com>
2026-06-29 01:15:17 +0900

    feat: 土日セルを薄く色付け(日=赤/土=青)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'summitsirdar-v13';
+const CACHE = 'summitsirdar-v14';
 const ASSETS = ['./', './manifest.json', './icon-192.png', './icon-512.png', './bg.png'];
 
 self.addEventListener('install', e => {
