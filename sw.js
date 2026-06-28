e03c5b134d2a5474d469cb1ed72c828165fc5a3a
izumoriluik <izumoriluik@gmail.com>
2026-06-28 20:41:08 +0900

    feat: デザインタブによるUI磨き(カレンダーピル・全体仕上げ)を採用

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'summitsirdar-v4';
+const CACHE = 'summitsirdar-v5';
 const ASSETS = ['./', './manifest.json', './icon-192.png', './icon-512.png', './bg.png'];
 
 self.addEventListener('install', e => {
